de482302ba125c311c7cb7307c263639e8b419e1
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-05 16:20:08 +0100

    finish 1st snack

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+const initNames = (namesList) => {
+    let initList = [""]
+    for (let i = 0; i < names.length; i++){
+            initList[i] = namesList[i].charAt(0)
+            
+        }
+    return initList
+    }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initNames(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
