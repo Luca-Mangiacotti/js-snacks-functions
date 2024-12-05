2736c1581b01ff64e3f60d3efb1fcaed3eb6c751
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-05 17:17:20 +0100

    finish bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,31 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const doGreet = (yourName) => {
+    let greet = (``)
+    let currentDate = new Date()
+    let currentHour = currentDate.getHours()
+    
+    if( currentHour >=6 && currentHour <= 13){
+        greet = (`Buongiorno ${yourName}`)
+    }else if( currentHour >= 14 && currentHour <= 17 ) {
+        greet = (`Buon Pomeriggio ${yourName}`)
+    }else if ( currentHour >= 18 && currentHour <= 22 ) {
+        greet = (`Buonasera ${yourName}`)
+    }else {
+        greet = (`Buonanotte ${yourName}`)
+    }
+
+    return greet
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
-
+console.log( doGreet(userName) )
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
