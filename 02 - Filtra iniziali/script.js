4fc94d4660ffa1b0be901564f38f1b23b8f4069e
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-05 16:29:31 +0100

    finish 2nd  snack

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+const initNames = (namesList, initChar) => {
+    let initList = []
+    for (let i = 0; i < namesList.length; i++){
+            if (initChar === namesList[i].charAt(0)){
+                initList.push(namesList[i])
+            }
+            
+        }
+    return initList
+    }
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initNames(names, "A"))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
