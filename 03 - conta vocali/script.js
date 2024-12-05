91748e8bd694af3943f32feaf30ddc7322b2748c
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-05 16:44:39 +0100

    finish 3rd snack

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,22 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const vowelsCounter = (word) => {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let counter = 0
+    for (let i = 0 ; i < word.length; i++){
+        for (let n = 0; n < vowels.length; n++){
+            if (word[i] === vowels[n]){
+                counter++
+            }
+        }
+    }
+    return counter
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log( vowelsCounter(word) )
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
